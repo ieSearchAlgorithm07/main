aea798f8fd0fa989aa53366a48219a71a9152572
Natsuki Sawasaki <e135719@Sawasaki-Natsuki-no-MacBook-Pro-2.local>
2015-01-04 15:29:53 +0900

    alpha update

--- alpha.sh
+++ alpha.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 alpha=$1
-while [ $alpha -le $3 ]
+sum=1
+sum=`echo "$2/$3"|bc -l`
+i=0
+echo $sum
+while true
 do
 cat steepest_decent_test.c  | sed "s/alpha =.*/alpha = $alpha;/" > steepest_decent.c
 make
 echo "alpha = $alpha"
 ./run_ave.sh
 sleep 1
-alpha=`expr $alpha + $2`
+alpha=`expr "$alpha + $2"|bc -l`
+i=`expr $i + 1`
+
+X=`echo "$3 > $alpha" | bc`
+if [ $X -eq 0 ] ;then
+	break;
+fi
+
 done
+echo "finish"
